fe06caf5487d9239171a075e0f5e2e14d916d8a7
Steve Bandarker <steve020629@icloud.com>
2023-10-31 11:34:12 +0200

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana, Lustria } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const lustiana = Lusitana({ subsets: ["latin"], weight: "700" });
